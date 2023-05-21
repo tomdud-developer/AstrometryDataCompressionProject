81ce7114b1a1f5ca8e877b7e051ca418bea0f371
tomdud-developer <tomadud045@student.polsl.pl>
2023-05-21 18:03:30 +0200

    Create Enum CompressMethod

--- src/main/java/org/astronomydatacompression/compression/CompressMethod.java
+++ src/main/java/org/astronomydatacompression/compression/CompressMethod.java
@@ -1,5 +1,15 @@
 package org.astronomydatacompression.compression;
 
 public enum CompressMethod {
-    M03,
+    M03(CompressM03.class);
+
+    private final Class<? extends Compress> compressClass;
+
+    CompressMethod(Class<? extends Compress> compressClass) {
+        this.compressClass = compressClass;
+    }
+
+    public Class<? extends Compress> getCompressClass() {
+        return compressClass;
+    }
 }
